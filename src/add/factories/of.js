833f8d6a9e3fb4b2989b2b1d68efb5f1251c6a40
Peter Shev <piotr.sevcenco@endava.com>
2017-03-28 21:01:27 +0300

    fix(of factory): Completes a stream after all values are emmited
    #1

--- src/add/factories/of.js
+++ src/add/factories/of.js
@@ -2,5 +2,8 @@ import './create'
 import statics from '../../statics'
 
 statics.of = function of(...xs) {
-  return statics.create({start: consumer => xs.forEach(i => consumer.next(i))}, 'of')
+  return statics.create({start: consumer => {
+    xs.forEach(i => consumer.next(i))
+    consumer.complete()
+  }}, 'of')
 }
