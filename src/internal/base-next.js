d2bc18fcb55a5fae52ae5b3e23e84c18b4206d7e
Peter Shev <piotr.sevcenco@endava.com>
2018-02-26 16:16:24 +0200

    perf(base next): adds a few if statements to improve hot path performance

--- src/internal/base-next.js
+++ src/internal/base-next.js
@@ -1,8 +1,21 @@
 export function baseNext(stream, x) {
   stream.hasEmitted = true
   stream.val = x
-  stream.subscribers.forEach(s => s.next(x))
-  stream.dependents.forEach(s => s.next(x))
+
+  if (stream.subscribers.length) {
+    if (stream.subscribers.length === 1)
+      stream.subscribers[0].next(x)
+    else
+      stream.subscribers.forEach(s => s.next(x))
+  }
+
+  if (stream.dependents.length) {
+    if (stream.dependents.length === 1)
+      stream.dependents[0].next(x)
+    else
+      stream.dependents.forEach(s => s.next(x))
+  }
+
   return stream
 }
 
